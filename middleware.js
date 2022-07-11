95d7d699d7ccdab48ba273de62bdfe8406eb7fd4
kartik dhawan <kartik.dhawan@3pillarglobal.com>
2022-07-11 16:27:51 +0530

    middlewares

--- middleware.js
+++ middleware.js
@@ -4,6 +4,9 @@
 const express = require("express");
 const app = express();
 
+// importing the morgan middleware
+const morgan = require("morgan");
+
 // setting up the view engine - ejs
 app.set("view engine", "ejs");
 
@@ -15,6 +18,25 @@ app.set("view engine", "ejs");
 app.listen(3000, "localhost", () => {
   console.log("listening");
 });
+
+// creating a few middleware(s)
+
+app.use((req, res, next) => {
+  console.log("\nNEW REQUEST RECEIVED");
+  console.log("host: " + req.hostname);
+  console.log("path: " + req.path);
+  console.log("method: " + req.method);
+  next(); // what next() does is, it tells the middleware that the function has completely run and now its time to move to the next middleware
+  // it is necessary because, since we are not sending any response, the middleware feels lost with no idea what to do next
+});
+
+app.use((req, res, next) => {
+  console.log("\nIN THE NEXT MIDDLEWARE");
+  next();
+});
+
+// morgan middleware - used to log out details of the request
+app.use(morgan("dev")); // read more on the documentation of npm
 
 // routing
 
@@ -36,8 +58,6 @@ app.get("/", (req, res) => {
         "  aliquam consectetur reiciendis libero vero, nam quaerat quod possimus sequi?",
     },
   ];
-  console.log(blogs);
-
   res.render("home", { head: "home", blogs });
 });
 
