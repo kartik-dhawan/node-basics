9c1c871315c2f746931c162ba144f382f3fc3710
kartik dhawan <kartik.dhawan@3pillarglobal.com>
2022-07-13 21:53:19 +0530

    removed mongo db credentials

--- middleware.js
+++ middleware.js
@@ -4,11 +4,8 @@
 const express = require("express");
 const app = express();
 
-// importing mongoose
-const mongoose = require("mongoose");
-
-// importing the mongodb model and schema
-const Blog = require("./models/blog");
+// importing the morgan middleware
+const morgan = require("morgan");
 
 // setting up the view engine - ejs
 app.set("view engine", "ejs");
@@ -16,40 +13,61 @@ app.set("view engine", "ejs");
 // if we change the name of the 'views' folder
 // app.set('views', 'newFolderName')
 
+// listens for request
+
+app.listen(3000, "localhost", () => {
+  console.log("listening");
+});
+
+// creating a few middleware(s)
+
+app.use((req, res, next) => {
+  console.log("\nNEW REQUEST RECEIVED");
+  console.log("host: " + req.hostname);
+  console.log("path: " + req.path);
+  console.log("method: " + req.method);
+  next(); // what next() does is, it tells the middleware that the function has completely run and now its time to move to the next middleware
+  // it is necessary because, since we are not sending any response, the middleware feels lost with no idea what to do next
+});
+
+app.use((req, res, next) => {
+  console.log("\nIN THE NEXT MIDDLEWARE");
+  next();
+});
+
+// 'morgan' middleware - used to log out details of the request
+
+app.use(morgan("dev")); // read more on the documentation of npm
+
 // 'static' middleware to be make use of static files on browser
 
 app.use(express.static("public"));
 
-// connecting to mongo db
-const dbURI =
-  "mongodb+srv://shelby:shelby101@cluster0.fbb80.mongodb.net/nodetuts?retryWrites=true&w=majority";
-
-mongoose
-  .connect(dbURI)
-  .then(() => {
-    app.listen(3000, "localhost", () => {
-      // listens for request only when connection to db is made
-      console.log("listening");
-    });
-  })
-  .catch((err) => console.log(err));
-
 // routing
 
 app.get("/", (req, res) => {
-  res.redirect("/blogs");
+  const blogs = [
+    {
+      title: "lorem78",
+      snippet:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam, sapiente",
+    },
+    {
+      title: "lorem891",
+      snippet:
+        "recusandae tempore aperiam nihil atque maiores id soluta laudantium necessitatibus",
+    },
+    {
+      title: "lorem67",
+      snippet:
+        "  aliquam consectetur reiciendis libero vero, nam quaerat quod possimus sequi?",
+    },
+  ];
+  res.render("home", { head: "home", blogs });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { head: "about" });
-});
-
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("home", { head: "Blogs", blogs: result });
-    });
 });
 
 app.get("/blogs/create", (req, res) => {
